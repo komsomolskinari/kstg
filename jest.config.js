aa905af9f7111a9162bbde3b5bfd9b87f6613b73
Student Main <scncgz@gmail.com>
2019-06-18 09:51:52 +0800

    Manually collect coverage to avoid a bug in jest and ts-jest

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,5 @@ module.exports = {
     preset: 'ts-jest/presets/js-with-ts',
     testEnvironment: 'node',
     roots: ['<rootDir>'],
-    testRegex: [/test[0-9a-zA-Z\/_-]+\.ts$/],
-    collectCoverage: true,
-    forceCoverageMatch: ['**/*.ts'] // Workaround for coverage broken in --watch
+    testRegex: [/test[0-9a-zA-Z\/_-]+\.ts$/]
 };
